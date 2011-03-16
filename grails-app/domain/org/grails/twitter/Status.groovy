3efd8ad3447cdaeb3478028641f773fbe8f9a163
Eugenia Harris <eugenia_harris@yahoo.com>
2011-03-16 00:47:25 -0400

    Disallow blank status messages and set jms queue name from app name to make it clearer what's going on here.

--- grails-app/domain/org/grails/twitter/Status.groovy
+++ grails-app/domain/org/grails/twitter/Status.groovy
@@ -3,12 +3,20 @@ package org.grails.twitter
 import org.grails.twitter.auth.Person
 
 class Status {
+
+    def grailsApplication
+
     String message
     Person author
     Date dateCreated
 
+    static constraints = {
+        message blank:false
+    }
+
     transient jmsService
     transient afterInsert = {
-        jmsService.send 'grailstwitter.status', author.username
+        // Note '<app name>.<service name of listener>' (e.g., grailstwitter/status)
+        jmsService.send "${grailsApplication.metadata.getApplicationName()}.status", author.username
     }
 }
